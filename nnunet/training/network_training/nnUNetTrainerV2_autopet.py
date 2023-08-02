d5a37ebca37aa873d60379a0d12349cd8053e821
zhack47 <mesbah.zacharia@sfr.fr>
2023-08-02 16:00:49 +0200

    Im not tire, you're tired !

--- nnunet/training/network_training/nnUNetTrainerV2_autopet.py
+++ nnunet/training/network_training/nnUNetTrainerV2_autopet.py
@@ -63,7 +63,8 @@ class AutoPETNet(SegmentationNetwork):
         self.hidden_size = 768
         spatial_dims = 3
         self.proj_axes = (0, spatial_dims + 1) + tuple(d + 1 for d in range(spatial_dims))
-        self.parameters = list(self.network.parameters()) + list(self.cl_a) + list(self.cl_c) + list(self.cl_s) + list(self.classifier)
+        self.parameters = list(self.network.parameters()) + list(self.cl_a.parameters())
+        + list(self.cl_c.parameters()) + list(self.cl_s.parameters()) + list(self.classifier.parameters())
 
     def proj_feat(self, x, feat_size):
         self.proj_view_shape = list(feat_size) + [self.hidden_size]

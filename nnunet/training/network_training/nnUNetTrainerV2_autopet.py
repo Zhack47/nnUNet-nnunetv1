af9f90baed92c9a42346f38d05449262571c3280
zhack47 <mesbah.zacharia@sfr.fr>
2023-08-03 16:56:49 +0200

    lowering threshold

--- nnunet/training/network_training/nnUNetTrainerV2_autopet.py
+++ nnunet/training/network_training/nnUNetTrainerV2_autopet.py
@@ -117,11 +117,11 @@ class AutoPETNet(SegmentationNetwork):
         else:
             if isinstance(output, (tuple, list)):
                 for i in range(output[0].shape[0]):
-                    output[0][i] = output[0][i] * torch.argmax(classif, dim=1)[i]
+                    output[0][i] = output[0][i] * int(classif[i, 1]<.1)
                 return output
             else:
                 for i in range(output.shape[0]):
-                    output[i] = output[i] * torch.argmax(classif, dim=1)[i]
+                    output[i] = output[i] * int(classif[i, 1]<.1)
                 return output
 
 
